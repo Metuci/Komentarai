d20e5bbcdd3fe257125e430cda3ccb0479c5a7ac
Your Name <you@example.com>
2016-04-04 20:00:56 +0300

    įdėta admin panelė

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :articles
   get 'home' => 'static_pages#home'
   get 'about' => 'static_pages#about'
